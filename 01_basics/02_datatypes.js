173f2f30a5d5e3a8f1a887106ced11316bd36b7e
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-01 02:46:27 +0000

    new

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+
+let name = "Rahul"
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+// string => " "
+// boolean => true or false
+// null => standalone value
+// undefined =>
+    // symbol => unique identifier
+    // object => 
+        
+        console.log(typeof null); 
