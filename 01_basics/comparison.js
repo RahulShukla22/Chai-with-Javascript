79d82fca7ddd804ad6228da56752eb48151e6609
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-02 07:15:27 +0000

    camparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log (2 == 1);
+// console.log (2!= 1);
+
+console.log("2" > 1);
+console.log (2 > "1");
+
+console.log (null > 0);
+console.log (null == 0);
+console.log (null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+
+// === strict check check data type also with value
+
+console.log(2 === 2);
