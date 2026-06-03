7a110d141c3fd4b30fe8273c79dcf059f74456bd
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-03 05:56:32 +0000

    string with slice

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -4,3 +4,29 @@ const repoCount = 50
 //console.log( name  +  repoCount + "value")
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+// another way of creating string with object
+
+const gameName = new String('Rahul Shukla-rs')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('R'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(0,4);
+console.log(anotherString);
+
+// slice with negative indices
+console.log(gameName.slice(-4,));      // last 4 characters
+console.log(gameName.slice(-4, -1));  // last 4 chars excluding the last one
+
+console.log(gameName.slice(-8));
