1a821e78a3a2e22b0ed91cab01533b978ff9bfe4
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-06 05:55:04 +0000

    AR

--- 01_basics/date.js
+++ 01_basics/date.js
@@ -6,4 +6,18 @@ console.log(myDate.toDateString());
 console.log(myDate.toLocaleString());
 console.log(typeof myDate);
 
-let myCreatedDate = new Date(2025,0,23);
+//let myCreatedDate = new Date(2025,0,23);
+//console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date ("2026-01-22");
+console.log(myCreatedDate.toLocaleString());
+
+let myTimestamp = Date.now();
+//console.log(myTimestamp);
+//console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
