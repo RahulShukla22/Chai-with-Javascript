d348eb3d533c3aad4fab81f4d0d846b969510e1f
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-07 10:20:46 +0000

    A

--- 01_basics/Array2.js
+++ 01_basics/Array2.js
@@ -12,3 +12,8 @@ console.log(newArr);
 //spread operator
 const all_heros = [...marvel_heros,...dc_heros];
 console.log(all_heros);
+
+const anotherArray = [1,2,3,[4,5,6],7[6,7,[4,5]]];
+const real_another_array = anotherArray.flat(Infinity);
+
+console.log(real_another_array);
