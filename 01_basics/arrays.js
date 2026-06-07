ed8239fdaa827703c4b6bc56f563d9a225c4f081
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-07 03:56:54 +0000

    ar

---
+++ 01_basics/arrays.js
@@ -0,0 +1,15 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktimaan","krissh"]
+
+const myArr2 = new Array(1,2,3,4);
+console.log(myArr[0]);
+
+myArr.push(6);
+myArr.push(7);
+
+//pop method removing last value from array
+myArr.pop
+
+console.log(myArr);
