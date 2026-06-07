ce76ec25183e184ebed2a2c4800a013c5546a45b
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-07 11:19:57 +0000

    obj

--- 01_basics/Array2.js
+++ 01_basics/Array2.js
@@ -17,3 +17,15 @@ const anotherArray = [1,2,3,[4,5,6],7[6,7,[4,5]]];
 const real_another_array = anotherArray.flat(Infinity);
 
 console.log(real_another_array);
+console.log(real_another_array);
+
+console.log(Array.isArray("rahul"));
+console.log(Array.from("rahul"));
+
+console.log(Array.from({name : "Rahul"})) // interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
---
+++ 01_basics/object.js
@@ -0,0 +1,15 @@
+// singleton
+// object.create
+
+const { PureComponent } = require("react");
+
+
+// object literals
+const Jsuser = {
+    name : "Rahul",
+    age : 28,
+    location : PureComponent,
+    email : "rahul.shukla123@gmail.com"
+    islo
+}
+
