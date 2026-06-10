37c8448f84b6d5f07cbdc495576a585431559c91
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-10 11:05:15 +0000

    IF

---
+++ 01_basics/IIFE.js
@@ -0,0 +1,15 @@
+//IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+
+})();
+
+( () => {
+    console.log(`DB TWO CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB Three CONNECTED ${name}`);
+})('rahul');
