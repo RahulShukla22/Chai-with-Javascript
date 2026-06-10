888e3e13fdf5fae790d006c5b352eb0c8a3d2593
RahulShukla22 <rahul.shukla22@zohomail.in>
2026-06-10 09:19:12 +0000

    f

--- 01_basics/functions.js
+++ 01_basics/functions.js
@@ -32,3 +32,30 @@ function loginuserMessage(username = "sam"){
 }
 //console.log(loginuserMessage("Rahul"))
 console.log(loginuserMessage("hitesh"))
+
+
+function calculateCardPrice(val1,val2,...num1) {
+    return num1
+}
+
+//console.log(calculateCardPrice(200,400,500,1000));
+
+const user = {
+    username: "divit",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user);
+handleObject({
+    username: "sam",
+    price:  399
+})
+
+const myNewArray = [200,400,600,800]
+function returnsecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnsecondValue(myNewArray));
+console.log(returnsecondValue([200,400,600,800]));
